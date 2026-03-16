aee9ac3f12c22f5f8b58cf324d816426d042657d
ElonaJasari <s375275@oslomet.no>
2026-03-16 14:50:57 +0100

    Update health_themes.ts
    fortsettelse på tema helse oversettelse

--- livsmestring-app/lib/themes/health_themes.ts
+++ livsmestring-app/lib/themes/health_themes.ts
@@ -14,7 +14,6 @@ Countries and their languagecodes:
     Ukrainian: uk
     Urdu: ur
 */
-
 export const health_themes = [
     {
         id: "skeiv_verden",
@@ -76,18 +75,234 @@ export const health_themes = [
                     en: "Diversity",
                     ar: "التنوع",
                     fa: "تنوع",
+                    ku: "Pir-rengî",
+                    so: "Kala duwanaansho",
+                    es: "La Diversidad",
+                    sw: "Kutafautiana",
+                    ta: "",
+                    ti: "ብዙሕነት (ፍልልይነት)",
+                    tr: "Toplumsal Çeşitlilik",
+                    uk: "Різноманітність",
+                    ur: "تنوع",
+            }
+        }, {
+                id: "diskriminering",
+                title: {
+                    no: "Diskriminring",
+                    en: "Discrimination",
+                    ar: "التمييز العنصري",
+                    fa: "تبعیض",
+                    ku: "Cudakarî",
+                    so: "Takoorid (Kala eexasho)",
+                    es: "La Discriminación",
+                    sw: "Ubaguzi",
+                    ta: "",
+                    ti: "ምግላል",
+                    tr: "Ayrımcılık",
+                    uk: "Дискримінація",
+                    ur: "امتیازی سلوک",
+            }
+        }
+        ]
+    },
+    {
+        id: "vold_i_nare_relasjoner",
+        title: {
+            no: "Vold i nære relasjoner",
+            en: "",
+            ar: "",
+            fa: "",
+            ku: "",
+            so: "",
+            es: "",
+            sw: "", 
+            ta: "",
+            ti: "",
+            tr: "",
+            uk: "",
+            ur: "",
+        },
+        subthemes: [
+            {
+                id: "vold_introduksjon",
+                title: {
+                    no: "Introduksjon",
+                    en: "",
+                    ar: "",
+                    fa: "",
                     ku: "",
                     so: "",
                     es: "",
-                    sw: "",
+                    sw: "", 
                     ta: "",
                     ti: "",
                     tr: "",
                     uk: "",
                     ur: "",
-            }
-        }
+                }
+            }, 
+            {
+                id: "vold_nye_ord", // vi må muligens finne en bedre løsning på id-navn, da mange av de samme ordene går igjen i flere temaer. Kanskje id kan være en kombinasjon av tema og ord som skrevet nå, slik at det blir unikt?
+                title: {
+                    no: "Nye ord",
+                    en: "",
+                    ar: "",
+                    fa: "",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "", 
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+                }
+            },
+            {
+                id: "typer_vold",
+                title: {
+                    no: "Typer vold",
+                    en: "",
+                    ar: "",
+                    fa: "",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "", 
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+                }
+            }, 
+            {
+                id: "naere_relasjoner",
+                title: {
+                    no: "Nære relasjoner",
+                    en: "",
+                    ar: "",
+                    fa: "",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "", 
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+                }
+            }, 
+            {
+                id: "avvergingsplikt",
+                title: {
+                    no: "Avvergingsplikt",
+                    en: "",
+                    ar: "",
+                    fa: "",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "", 
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+                }
+            }, 
+            {
+                id: "taushetsplikt",
+                title: {
+                    no: "Taushetsplikt",
+                    en: "",
+                    ar: "",
+                    fa: "",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "", 
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+                }
+            }, 
+            {
+                id: "tegn_paa_vold",
+                title: {
+                    no: "Tegn på vold",
+                    en: "",
+                    ar: "",
+                    fa: "",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "", 
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+                }
+            }, 
+            {
+                id: "hvem_kan_hjelpe_1",
+                title: {
+                    no: "Hvem kan hjelpe, 1",
+                    en: "",
+                    ar: "",
+                    fa: "",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "", 
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+                }
+            }, 
+            {
+                id: "hvem_kan_hjelpe_2",
+                title: {
+                    no: "Hvem kan hjelpe, 2",
+                    en: "",
+                    ar: "",
+                    fa: "",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "", 
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+                }
+            }, 
+            {
+                id: "vold_case",
+                title: {
+                    no: "Case",
+                    en: "",
+                    ar: "",
+                    fa: "",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "", 
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+                }
+            }, 
         ]
-
     }
 ]
