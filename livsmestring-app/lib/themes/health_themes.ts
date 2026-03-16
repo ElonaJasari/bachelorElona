d9b75a7f99c1e72f3bd4c19ee25611f85389886d
ElonaJasari <s375275@oslomet.no>
2026-03-16 14:11:52 +0100

    startet på tema inndeling

---
+++ livsmestring-app/lib/themes/health_themes.ts
@@ -0,0 +1,93 @@
+/* 
+Countries and their languagecodes:
+    Norway: no
+    English: en
+    Arabic: ar
+    Farsi: fa
+    Kurmanji: ku
+    Somali: so
+    Spanish: es
+    Swahili: sw
+    Tamil: ta
+    Tigrinya: ti
+    Turkish: tr
+    Ukrainian: uk
+    Urdu: ur
+*/
+
+export const health_themes = [
+    {
+        id: "skeiv_verden",
+        title: {
+            no: "Skeiv verden",
+            en: "Queer world",
+            ar: "",
+            fa: "",
+            ku: "",
+            so: "",
+            es: "",
+            sw: "",
+            ta: "",
+            ti: "",
+            tr: "",
+            uk: "",
+            ur: "",
+
+        },
+        subthemes: [
+            {
+                id: "valgfrihet",
+                title: {
+                    no: "Valgfrihet",
+                    en: "Freedom of choice",
+                    ar: "حرية الاختيار",
+                    fa: "آزادی انتخاب",
+                    ku: "Azadiya hilbijartinê",
+                    so: "Xorriyada-doorashada",
+                    es: "La Libertad de Elección",
+                    sw: "Uhuru wa kuchagua",
+                    ta: "",
+                    ti: "ናይ ምምራጽ ናጽነት",
+                    tr: "Tercih Özgürlüğü",
+                    uk: "Свобода вибору",
+                    ur: "اختیار کا حق", 
+                }
+            }, {
+                id: "sosiale_normer",
+                title: {
+                    no: "Sosiale normer",
+                    en: "Social norms",
+                    ar: "الأعراف الاجتماعية",
+                    fa: "نورم‌های اجتماعی",
+                    ku: "Adatên civakî",
+                    so: "Xeerarka Bulshada",
+                    es: "Normas sociales",
+                    sw: "Kanuni za kijamii",
+                    ta: "",
+                    ti: "ማሕበራዊ ስርዓታት",
+                    tr: "Sosyal normlar",
+                    uk: "Соціальні норми",
+                    ur: "معاشرتی اصول", 
+                }
+            }, {
+                id: "mangfold",
+                title: {
+                    no: "Mangfold",
+                    en: "Diversity",
+                    ar: "التنوع",
+                    fa: "تنوع",
+                    ku: "",
+                    so: "",
+                    es: "",
+                    sw: "",
+                    ta: "",
+                    ti: "",
+                    tr: "",
+                    uk: "",
+                    ur: "",
+            }
+        }
+        ]
+
+    }
+]
--- livsmestring-app/lib/translations.ts
+++ livsmestring-app/lib/translations.ts
@@ -3,5 +3,5 @@ const translations = {
     'en': { selectLanguage: 'select language'},
     'no': { selectLanguage: 'velg språk'},
     'ar': { selectLanguage: 'اختر اللغة'},
-    'so': { selectLanguage: 'doorashada luqadda'},
+    'fa': { selectLanguage: 'انتخاب زبان'},
 }
