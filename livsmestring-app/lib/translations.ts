e722ac256c785b0feec2f7ab8b414c80bf534de4
ElonaJasari <s375275@oslomet.no>
2026-03-16 13:15:04 +0100

    made translations file.

---
+++ livsmestring-app/lib/translations.ts
@@ -0,0 +1,7 @@
+// translated strings for the website, used in the language selector and other places
+const translations = {
+    'en': { selectLanguage: 'select language'},
+    'no': { selectLanguage: 'velg språk'},
+    'ar': { selectLanguage: 'اختر اللغة'},
+    'so': { selectLanguage: 'doorashada luqadda'},
+}
