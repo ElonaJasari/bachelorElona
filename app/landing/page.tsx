5608b91ba311ab1edf6121b72854a65e2696d437
ElonaJasari <144010491+ElonaJasari@users.noreply.github.com>
2026-03-18 11:26:17 +0100

    Create landing page file

---
+++ app/landing/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div style={{ backgroundColor: 'white', padding: '20px', textAlign: 'center' }}>
+      <h1>Welcome to the Landing Page</h1>
+      <p>This is the landing page of the application. Here you can find more information about our offerings.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
